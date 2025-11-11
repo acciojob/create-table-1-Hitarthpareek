fcea32903b640e7a5b712c643249765c799a6b62
Hitarthpareek <80564705+Hitarthpareek@users.noreply.github.com>
2025-11-11 11:05:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+let count=0
 function insert_Row() {
     //Write your code here
   let cell1 = document.createElement("td")
   let cell2 = document.createElement("td")
-  cell1.textContent = "New Cell"
-  cell2.textContent = "New Cell2"
+  cell1.textContent = `New Cell${count+1}`
+  cell2.textContent = `New Cell${count+2}`
+  count++
   let row = document.createElement("tr")
   row.appendChild(cell1)
   row.appendChild(cell2)
